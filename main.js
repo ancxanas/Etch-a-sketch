bb25e9080a2c5fdb0a7b6d021b973d1fe9d0ac20
ancxanas <anas.corona.github@gmail.com>
2022-10-25 16:14:35 +0530

    function to create a grid

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const container = document.getElementById('container');
 
+function makeGrid(value){
+    for(let i = 0; i < value; i++){
+        for(let j = 0; j < value; j++){
+        let cell = document.createElement('div');
+        container.style.gridTemplateColumns = `repeat(${value}, 20px)`;
+        container.style.gridTemplateRows = `repeat(${value}, 20px)`;
+        cell.setAttribute('style','border: 1px solid black');
+        container.appendChild(cell);
+        }
+    }
+}
 
-const div = document.createElement('div');
+makeGrid(16);
 
