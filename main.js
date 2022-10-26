53581b37bfd7716d2aec389c0dcfe86e65837db4
ancxanas <anas.corona.github@gmail.com>
2022-10-26 15:40:17 +0530

    Added a div for grid

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 const container = document.getElementById('container');
 
+const grid = document.createElement('div');
+grid.classList.add('grid');
+container.appendChild(grid);
+
+//function to create grid
 function makeGrid(value){
+    grid.style.gridTemplateColumns = `repeat(${value}, 20px)`;
+    grid.style.gridTemplateRows = `repeat(${value}, 20px)`;
     for(let i = 0; i < value; i++){
         for(let j = 0; j < value; j++){
-        let cell = document.createElement('div');
-        container.style.gridTemplateColumns = `repeat(${value}, 20px)`;
-        container.style.gridTemplateRows = `repeat(${value}, 20px)`;
-        cell.setAttribute('style','border: 1px solid black');
-        container.appendChild(cell);
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.setAttribute('style','border: 1px solid #000000');
+        cell.addEventListener('mouseover', toBlack);
+        cell.addEventListener('mousedown', eraser);
+        grid.appendChild(cell);
         }
     }
 }
 
 makeGrid(16);
 
+function toBlack(e){
+    e.target.style.backgroundColor = 'black';
+}
+
+function eraser(e){
+    e.target.style.backgroundColor = 'white';
+}
