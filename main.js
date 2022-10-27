bf00c1b73e6673e25ca73eda7bf499b55b3d2450
ancxanas <anas.corona.github@gmail.com>
2022-10-27 17:52:33 +0530

    Modified the grid to be same with different values

--- main.js
+++ main.js
@@ -1,31 +1,49 @@
 const container = document.getElementById('container');
 
+
+
+//div for buttons
+const buttonDiv = document.createElement('div');
+buttonDiv.classList.add('button-div');
+container.appendChild(buttonDiv);
+
+//A button to reset Everything
+const resetButton = document.createElement('button');
+resetButton.textContent = 'Reset';
+buttonDiv.appendChild(resetButton);
+
+//grid in a seperate div
 const grid = document.createElement('div');
 grid.classList.add('grid');
 container.appendChild(grid);
 
 //function to create grid
 function makeGrid(value){
-    grid.style.gridTemplateColumns = `repeat(${value}, 20px)`;
-    grid.style.gridTemplateRows = `repeat(${value}, 20px)`;
+    let gridWidth = (grid.offsetWidth / value) - getComputedStyle(grid).gap.slice(0, -2);
+    grid.style.gridTemplateColumns = `repeat(${value}, ${gridWidth}px)`;
+    grid.style.gridTemplateRows = `repeat(${value}, ${gridWidth}px)`;
     for(let i = 0; i < value; i++){
         for(let j = 0; j < value; j++){
         const cell = document.createElement('div');
         cell.classList.add('cell');
-        cell.setAttribute('style','border: 1px solid #000000');
-        cell.addEventListener('mouseover', toBlack);
-        cell.addEventListener('mousedown', eraser);
+        cell.addEventListener('mouseover', toColor);
         grid.appendChild(cell);
         }
     }
 }
 
-makeGrid(16);
 
-function toBlack(e){
-    e.target.style.backgroundColor = 'black';
+//to change the cell color to black on mouseover
+function toColor(e){
+    e.target.style.backgroundColor = '#333333';
 }
 
-function eraser(e){
-    e.target.style.backgroundColor = 'white';
+function resetGrid(){
+    const value = prompt('Input a number of Squares');
+    grid.innerHTML = '';
+    makeGrid(value);
 }
+
+resetButton.addEventListener('click',resetGrid);
+
+window.onload = () => {makeGrid(16)};
